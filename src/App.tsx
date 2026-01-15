d6f77aee0f334f16bd3c85918ef375ca08d9ad46
Amir Greenberg <amir.greenberg@t-a9.com>
2026-01-15 11:18:51 +0200

    Fix base path to work with both Vercel and GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/amir-s-digital-showcase">
+      <BrowserRouter basename={import.meta.env.BASE_URL}>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
