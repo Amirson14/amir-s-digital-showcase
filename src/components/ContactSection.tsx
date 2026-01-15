a469196309a8a2d615dd41319d2f9207d2a7c982
Amir Greenberg <amir.greenberg@t-a9.com>
2026-01-15 14:27:23 +0200

    Remove consultation button from ContactSection

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -119,21 +119,6 @@ const ContactSection = () => {
               );
             })}
           </motion.div>
-
-          {/* CTA */}
-          <motion.div
-            initial={{ opacity: 0, y: 30 }}
-            animate={isInView ? { opacity: 1, y: 0 } : {}}
-            transition={{ duration: 0.6, delay: 0.5 }}
-            className="text-center mt-12"
-          >
-            <a
-              href="mailto:gr.amir@gmail.com?subject=פנייה מהאתר - שיחת ייעוץ"
-              className="btn-gold text-lg inline-block"
-            >
-              קבעו שיחת ייעוץ
-            </a>
-          </motion.div>
         </div>
       </div>
     </section>
